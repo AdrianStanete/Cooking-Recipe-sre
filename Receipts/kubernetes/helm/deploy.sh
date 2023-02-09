52cc444943bca7a5d57eb7de9e1be6ca04de8f75
Adrian Stanete <adrianstanete@gmail.com>
2023-02-09 17:10:18 +0100

    added database and it's secrets

--- Receipts/kubernetes/helm/deploy.sh
+++ Receipts/kubernetes/helm/deploy.sh
@@ -1,3 +1,4 @@
 kubectl apply -f cookingapp-namespace.yml
-kubectl apply -f postgres-secret.yml
+kubectl -n cookingapp apply -f secret.yaml
 helm -n cookingapp upgrade --install -f values.yaml postgres bitnami/postgresql
+kubectl -n cookingapp apply -f receipts-deployment.yml
